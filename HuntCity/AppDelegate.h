18fd814935b5b726d11a6e6b69ae93aefcae19a3
hongjunxiao <hongjunxiao@meilishuo.com>
2015-11-25 15:40:18 +0800

    CREATE

---
+++ HuntCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuntCity
+//
+//  Created by hongjunxiao on 15/11/25.
+//  Copyright © 2015年 XHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuntCity/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HuntCity
+//
+//  Created by hongjunxiao on 15/11/25.
+//  Copyright © 2015年 XHJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"HELLO");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
